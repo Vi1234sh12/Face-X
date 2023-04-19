89b16ca735a38abe498eefd5f490922188a6dddb
PSY222 <ss26348@naver.com>
2023-04-19 22:11:40 +0900

    Added minion eyes filter folder
    Signed-off-by: PSY222 <ss26348@naver.com>

---
+++ Snapchat_Filters/MinionEyes Filter/README.md
@@ -0,0 +1,17 @@
+### Make a Simple and Fun Minions Filter with OpenCV
+
++ Real time Minions eye filter using simple OpenCV codes
++ Used CascadeClassifier instead of Dlib for efficient real-time application
+
+image.png
+
+### Getting started
+1. Clone this repository and navigate to this folder
+2. run face_filter.py file
+'''
+python face_filter.py
+'''
+3. Enjoy cute and fun Minions filter!😍
+
+(If you want to develop this filter further, feel free to make advanced filter that minion eyes can follow your gaze)
+
---
+++ Snapchat_Filters/MinionEyes Filter/face_filter.py
@@ -0,0 +1,67 @@
+# Face filters (Snapchat like) using OpenCV
+# @author:PSY222
+
+import cv2
+import sys
+import datetime as dt
+from time import sleep
+import numpy as np
+import os
+
+current_dir = os.path.dirname(__file__)
+image_path = os.path.join(current_dir, 'minion_eyes.png')
+cascPath = os.path.join(current_dir, 'frontalEyes.xml')  # for eye detection
+
+if os.path.exists(cascPath):
+    print('Loading camera now')
+else:
+    print('CascPath does not exists')
+
+eyeCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+anterior = 0
+eyes = cv2.imread(image_path)
+
+
+def put_eyes(eyes,fc,x,y,w,h):
+    eye_width,eye_height = w, h
+    eye = cv2.resize(eyes,(w,h))
+
+    for i in range(eye.shape[0]):
+         for j in range(eye.shape[1]):
+            for k in range(3):
+                if not np.all(eye[i,j,k]==[0,0,0]):
+                    fc[y+i,x+j][k] = eye[i,j][k]
+    return fc
+    
+    
+while True:
+    if not video_capture.isOpened():
+        print('Unable to load camera.')
+        sleep(5)
+        pass
+
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    eye_loc = eyeCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(40,40)
+    )
+
+    for (x, y, w, h) in eye_loc:
+            frame = put_eyes(eyes,frame,x,y,w,h)
+            
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+video_capture.release()
+cv2.destroyAllWindows()
